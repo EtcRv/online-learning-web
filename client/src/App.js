0572cf53b0b57c6586e7e206c0e3b316a8863000
DungCodeDao <dangdung060802@gmail.com>
2023-05-09 09:46:51 +0700

    ui_home_page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage/HomePage";
 import LoginPage from "./Pages/AuthenticationPage/LoginPage";
 import Layout from "./Components/Layout/Layout";
+import SignUpPage from "./Pages/AuthenticationPage/SignUpPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<HomePage />} />
           <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
